f4fbeb999505776a930a306a4b75ee1ab128e002
Victor Suzdalev <victor.suzdalev@gmail.com>
2021-01-12 14:02:26 +0300

    fix typo in tests

--- tests/test_recover_erc20.py
+++ tests/test_recover_erc20.py
@@ -14,21 +14,21 @@ def test_owner_recovers_erc20_without_balance(rewards_manager, ldo_token, ape):
 
 
 def test_owner_recovers_erc20_with_balance(rewards_manager, ldo_token, dao_agent, ape, stranger):
-    recepient = stranger
+    recipient = stranger
     transfer_amount = brownie.Wei("1 ether")
     ldo_token.transfer(rewards_manager, transfer_amount, {"from": dao_agent})
     assert ldo_token.balanceOf(rewards_manager) == transfer_amount
 
-    recepient_balance_before = ldo_token.balanceOf(recepient)
-    tx = rewards_manager.recover_erc20(ldo_token, recepient, {"from": ape})
-    recepient_balance_after = ldo_token.balanceOf(recepient)
+    recipient_balance_before = ldo_token.balanceOf(recipient)
+    tx = rewards_manager.recover_erc20(ldo_token, recipient, {"from": ape})
+    recipient_balance_after = ldo_token.balanceOf(recipient)
 
     assert ldo_token.balanceOf(rewards_manager) == 0
-    assert recepient_balance_after - recepient_balance_before == transfer_amount
+    assert recipient_balance_after - recipient_balance_before == transfer_amount
     assert len(tx.events) == 1
     assert tx.events[0].name == "Transfer"
     assert tx.events[0]["_from"] == rewards_manager
-    assert tx.events[0]["_to"] == recepient
+    assert tx.events[0]["_to"] == recipient
     assert tx.events[0]["_value"] == transfer_amount
 
 
@@ -36,12 +36,12 @@ def test_owner_recovers_erc20_to_the_caller_by_default(rewards_manager, ldo_toke
     transfer_amount = brownie.Wei("1 ether")
     ldo_token.transfer(rewards_manager, transfer_amount, {"from": dao_agent})
 
-    recepient_balance_before = ldo_token.balanceOf(ape)
+    recipient_balance_before = ldo_token.balanceOf(ape)
     tx = rewards_manager.recover_erc20(ldo_token, {"from": ape})
-    recepient_balance_after = ldo_token.balanceOf(ape)
+    recipient_balance_after = ldo_token.balanceOf(ape)
 
     assert ldo_token.balanceOf(rewards_manager) == 0
-    assert recepient_balance_after - recepient_balance_before == transfer_amount
+    assert recipient_balance_after - recipient_balance_before == transfer_amount
     assert len(tx.events) == 1
     assert tx.events[0].name == "Transfer"
     assert tx.events[0]["_from"] == rewards_manager
